e38850c290cee941d6a70572f3a545fe94e4eb82
geckotang <macky1207@gmail.com>
2019-11-30 15:05:51 +0900

    feat(markups): マークアップごとにreadmeを設置する

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
+const markdownIt = require('markdown-it');
+const markdownItOptions = {
+  html: true,
+  breaks: true,
+  linkify: true,
+};
+
 module.exports = function(config) {
+
+  config.addFilter('markdown', function(value) {
+    const md = markdownIt(markdownItOptions);
+    return md.render(value);
+  });
+
   return {
     dir: {
       input: 'src/site',
@@ -7,4 +20,5 @@ module.exports = function(config) {
       output: 'dist',
     }
   };
+
 };
